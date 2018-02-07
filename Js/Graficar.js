65544b2e9ec9aa73d61fb97f29abe0f7267d029c
Loyalknight <loyalknight087@gmail.com>
2018-02-07 10:02:54 -0500

    Ya se capturan los datos con jquery

---
+++ Js/Graficar.js
@@ -0,0 +1,13 @@
+
+$(document).ready(metodos);
+
+function metodos() {
+	$("#Plot").click(capturarDatos);	
+ }
+
+function capturarDatos() {
+	var frecuencia = $("#F").val();
+	var valorinicial = $("#ID").val();
+	var valorfinal = $("#FD").val();
+	var unidad= $("#U").val();
+}
