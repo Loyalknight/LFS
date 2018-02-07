b66f1c426be1928e5e37a6900c84a3af4ae6d487
Loyalknight <loyalknight087@gmail.com>
2018-02-07 17:05:46 -0500

    Ya funciona la primera opcion , se validan los campos , y ya grafica

--- Js/Graficar.js
+++ Js/Graficar.js
@@ -2,45 +2,88 @@
 $(document).ready(metodos);
 
 function metodos() {
-	$("#Plot").click(capturarDatos);
-
-	new Morris.Line({
-  
-  	element: 'myfirstchart',
-
-  	data: [
-    	{ year: '2008', value: 20 },
-    	{ year: '2009', value: 10 },
-    	{ year: '2010', value: 5 },
-    	{ year: '2011', value: 5 },
-    	{ year: '2012', value: 20 }
-  	],
-  
-  	xkey: 'year',
-  	ykeys: ['value'],
- 	labels: ['Value'],
- 	resize: true
-
-	});	
+	$("#Plot").click(validar);
  }
 
-function capturarDatos() {
+function validar(){
 
-	var constantekmMHz=32.4;
-	var constanteKmGHz=92.4;
 	var frecuencia = $("#F").val();
 	var valorinicial = $("#ID").val();
 	var valorfinal = $("#FD").val();
+
+	if (frecuencia == "" || valorinicial == "" || valorfinal == "") {
+        $.toast({	
+    		text: 'Debe llenar todos los campos',
+    		position: 'top-center'	
+		})
+        return false;
+    }else if(parseFloat(frecuencia)<0 || parseFloat(valorinicial)<0 || parseFloat(valorfinal)<0){
+    	$.toast({	
+    		text: 'No pueden haber valores negativos',
+    		position: 'top-center'	
+		})
+        return false;
+    }else if(parseFloat(valorinicial)>parseFloat(valorfinal)){
+		$.toast({	
+    		text: 'El valor inicial debe ser menor que el final',
+    		position: 'top-center'	
+		})
+        return false;
+    }else{
+	capturarDatos();
+    }
+
+}
+
+function capturarDatos() {
+	
+	var g="";
+	g+='<div id="myfirstchart"></div>';
+	$("#grafica").html(g);
+
+	
+	var constantekmMHz=32.4;
+	var constanteKmGHz=92.4;
+	var frecuencia = parseFloat($("#F").val());
+	var valorinicial = parseFloat($("#ID").val());
+	var valorfinal = parseFloat($("#FD").val());
 	var unidad= $("#U").val();
 	var n = unidad.localeCompare("GHz");
-	var distanciaLog= 20*Math.log10(frecuencia);
-	alert(distanciaLog);
+	var frecuenciaLog= 20*Math.log10(frecuencia);
 
-	/*
-	if (n==0) {
-		alert("paso")
+	if(n==0){
+		var info= [{ year: valorinicial, value: constanteKmGHz+(20*Math.log10(valorinicial))+frecuenciaLog }]; 
 	}else{
-		alert("no paso");
+		var info= [{ year: valorinicial, value: constantekmMHz+(20*Math.log10(valorinicial))+frecuenciaLog }]; 
 	}
-	*/
+	
+	
+
+	for (var i = valorinicial+1; i <=valorfinal; i++) {
+		if(n==0){
+			info.push({
+			"year": i +'Km',
+	    	"value":constanteKmGHz+(20*Math.log10(i))+frecuenciaLog
+			});
+		}else{
+			info.push({
+			"year": i+'Km',
+	    	"value":constantekmMHz+(20*Math.log10(i))+frecuenciaLog
+			});
+		}
+	}
+
+	Morris.Line({
+  
+  	element: 'myfirstchart',
+  	data: info,
+  	xkey: 'year',
+  	ykeys: ['value'],
+ 	labels: ['P.E.L'],
+ 	resize: true,
+ 	parseTime: false,
+ 	postUnits: 'dB'
+ 	
+	});	
+
 }
